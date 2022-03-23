9994b092fa8eaabad666f0e19d57cedd7b2d6317
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-03-23 14:39:25 +0100

    initial commit, skeleton

---
+++ main.js
@@ -0,0 +1,48 @@
+const datas = [
+    {
+        title: "US Dollar",
+        sub: "$",
+        text: "USD"
+    },
+    {
+        title: "Euro",
+        sub: "E",
+        text: "EUR"
+    },
+    {
+        title: "Thai Baht",
+        sub: "$",
+        text: "THB"
+    },
+    {
+        title: "British Pound Sterling",
+        sub: "$",
+        text: "GBP"
+    },
+    {
+        title: "Ghanaian Cedi",
+        sub: "$",
+        text: "GHS"
+    },
+    {
+        title: "Japanese Yen",
+        sub: "$",
+        text: "JPY"
+    },
+    {
+        title: "Polish Zloty",
+        sub: "$",
+        text: "PLN"
+    },
+    {
+        title: "US Dollar",
+        sub: "$",
+        text: "USD"
+    },
+]
+
+function loadEvent() {
+    console.log('the page has loaded');
+}
+
+window.addEventListener("load", loadEvent);
